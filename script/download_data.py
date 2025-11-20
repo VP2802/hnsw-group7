5cec0e49b6bc0278d57e2142d74164e49db0911c
thuatvn28 <thuatvn28@gmail.com>
2025-11-20 23:14:01 +0700

    new change in download_data

--- script/download_data.py
+++ script/download_data.py
@@ -11,7 +11,7 @@ def main():
     print("TẢI PRE-BUILT DATA")
     print("=" * 30)
     
-    DOWNLOAD_URL = "https://github.com/VP2802/hnsw-group7/releases/download/v1.0.0/prebuilt_data.zip"
+    DOWNLOAD_URL = "https://github.com/VP2802/hnsw-group7/releases/download/v1.0.1/article_index.zip"
     
     print("Đang tải data từ GitHub...")
     
@@ -19,13 +19,13 @@ def main():
         response = requests.get(DOWNLOAD_URL)
         response.raise_for_status()
         
-        with open("prebuilt_data.zip", "wb") as f:
+        with open("article_index.zip", "wb") as f:
             f.write(response.content)
         
         print("Download xong!")
         
         print("Đang giải nén...")
-        with zipfile.ZipFile("prebuilt_data.zip", "r") as zip_ref:
+        with zipfile.ZipFile("article_index.zip", "r") as zip_ref:
             zip_ref.extractall("article_index")
         
         os.remove("prebuilt_data.zip")
